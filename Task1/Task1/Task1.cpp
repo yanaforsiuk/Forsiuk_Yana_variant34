a06f0cdb31c94e82e5c8ed67ccc94bb05568282d
yanaforsiuk <yana.forsiuk@oa.edu.ua>
2026-04-14 16:33:49 +0300

    Update Task1.cpp

--- Task1/Task1/Task1.cpp
+++ Task1/Task1/Task1.cpp
@@ -1,20 +1,127 @@
-// Task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <future>
+#include <cstdlib>
+#include <ctime>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+void generateArray(vector<int>& arr, int size) {
+    arr.resize(size);
+    for (int& x : arr)
+        x = rand() % 10000;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void printArray(const vector<int>& arr) {
+    for (int x : arr)
+        cout << x << " ";
+    cout << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+
+void countingSort(vector<int>& arr) {
+    int maxVal = *max_element(arr.begin(), arr.end());
+    vector<int> count(maxVal + 1, 0);
+
+    for (int x : arr)
+        count[x]++;
+
+    int index = 0;
+    for (int i = 0; i <= maxVal; i++) {
+        while (count[i]--) {
+            arr[index++] = i;
+        }
+    }
+}
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int binarySearch(const vector<int>& arr, int target) {
+    int left = 0, right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    srand(time(0));
+
+    vector<int> sizes = { 8, 80, 1200, 4000, 25000, 100000 };
+
+    for (int size : sizes) {
+        vector<int> arr;
+        generateArray(arr, size);
+
+        vector<int> bubble = arr;
+        vector<int> counting = arr;
+        vector<int> quick = arr;
+
+        cout << "\nArray size = " << size << endl;
+
+        auto start = chrono::high_resolution_clock::now();
+        bubbleSort(bubble);
+        auto end = chrono::high_resolution_clock::now();
+        cout << "Bubble Sort: "
+            << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+            << " ms\n";
+
+        start = chrono::high_resolution_clock::now();
+        countingSort(counting);
+        end = chrono::high_resolution_clock::now();
+        cout << "Counting Sort: "
+            << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+            << " ms\n";
+
+        start = chrono::high_resolution_clock::now();
+        quickSort(quick, 0, quick.size() - 1);
+        end = chrono::high_resolution_clock::now();
+        cout << "Quick Sort: "
+            << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+            << " ms\n";
+    }
+
+    return 0;
+}
